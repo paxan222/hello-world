90be9ad72778860258b378d18a0ea7458c956378
paxan222 <paxan222@yandex.ru>
2016-06-22 16:35:50 +0300

    update

--- LocalizationParser/LocalizationParser/MainWindow.xaml.cs
+++ LocalizationParser/LocalizationParser/MainWindow.xaml.cs
@@ -1,20 +1,13 @@
 ﻿using System;
-using System.Collections.Generic;
+using System.Collections;
+using System.Diagnostics;
 using System.IO;
-using System.Linq;
-using System.Text;
+using System.Runtime.InteropServices;
+using System.Text.RegularExpressions;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Forms;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
 using System.Windows.Media.TextFormatting;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-using Microsoft.Win32;
+using Excel=Microsoft.Office.Interop.Excel;
 
 namespace LocalizationParser
 {
@@ -26,19 +19,12 @@ namespace LocalizationParser
         public MainWindow()
         {
             InitializeComponent();
+            TextBlock.Text = Directory.GetParent(FileProperties.Path).FullName;
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-            //OpenFileDialog openFileDialog = new OpenFileDialog();
-            //openFileDialog.InitialDirectory = FileProperties.Path;
-            //if (openFileDialog.ShowDialog() == true)
-            //{
-            //    FileProperties.Path = openFileDialog.InitialDirectory;
-            //    FileProperties.FileName = openFileDialog.FileName;
-            //    TextBlock.Text = FileProperties.FileName;
-            //}
-            FolderBrowserDialog dialog = new FolderBrowserDialog();
+            var dialog = new FolderBrowserDialog();
             dialog.RootFolder = Environment.SpecialFolder.MyComputer;
             if (dialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
             {
@@ -51,72 +37,158 @@ namespace LocalizationParser
         {
             string[] dirStrings = Directory.GetFiles(FileProperties.Path,"*.cs", SearchOption.AllDirectories);
             TextBox.Text = string.Empty;
-            Parse parse = new Parse();
-            StreamWriter streamWriter = new StreamWriter("D:\\someRussianText.txt");
+            var parse = new Parse();
+            var directoryInfo = new DirectoryInfo(FileProperties.RootPath);
+            if (directoryInfo.Exists)
+            {
+                foreach (FileInfo file in directoryInfo.GetFiles())
+                {
+                    file.Delete();
+                }
+            }
             foreach (var dirString in dirStrings)
             {
-                streamWriter.WriteLine(System.IO.Path.GetFileName(dirString) + "\t" + parse.Output(new StreamReader(dirString)));
-                //TextBox.Text += dirString + Environment.NewLine + Environment.NewLine;
-                //TextBox.Text += parse.Output(new StreamReader(dirString));
+                string path = FileProperties.RootPath + Directory.GetParent(dirString).Name;
+                parse.Writer(path,dirString,TextBox);
             }
-            streamWriter.Close();
         }
     }
 
     public class FileProperties
     {
-        public static string Path = "D:\\repo\\Rubezh\\Projects\\FireAdministrator\\Modules\\AutomationModule\\Steps";
+        public static string Path = "D:\\repo\\Trank\\Projects\\FireAdministrator\\Modules\\AutomationModule\\";
+        public static string RootPath = "D:\\repo\\LocalizedTrank\\";
         public static string FileName = string.Empty;
     }
 
     public class Parse
     {
+        Hashtable _myHashtable;
         public string Output(StreamReader streamReader)
         {
-            string tmp = "";
+            string tmp = string.Empty;
             while (!streamReader.EndOfStream)
             {
                 string tmp2 = streamReader.ReadLine();
-                string line = tmp2.ToLower();
-                if (line != null && (line.Contains("а") ||
-                                     line.Contains("б") ||
-                                     line.Contains("в") ||
-                                     line.Contains("г") ||
-                                     line.Contains("д") ||
-                                     line.Contains("е") ||
-                                     line.Contains("ё") ||
-                                     line.Contains("ж") ||
-                                     line.Contains("з") ||
-                                     line.Contains("и") ||
-                                     line.Contains("й") ||
-                                     line.Contains("к") ||
-                                     line.Contains("л") ||
-                                     line.Contains("м") ||
-                                     line.Contains("н") ||
-                                     line.Contains("о") ||
-                                     line.Contains("п") ||
-                                     line.Contains("р") ||
-                                     line.Contains("с") ||
-                                     line.Contains("т") ||
-                                     line.Contains("у") ||
-                                     line.Contains("ф") ||
-                                     line.Contains("х") ||
-                                     line.Contains("ц") ||
-                                     line.Contains("ч") ||
-                                     line.Contains("ш") ||
-                                     line.Contains("щ") ||
-                                     line.Contains("ь") ||
-                                     line.Contains("ы") ||
-                                     line.Contains("ъ") ||
-                                     line.Contains("э") ||
-                                     line.Contains("ю") ||
-                                     line.Contains("я")))
+                if (tmp2 != null)
                 {
-                    tmp += tmp2 + Environment.NewLine;
+                    string line = tmp2.ToLower();
+                    if ((line.Contains("а") ||
+                         line.Contains("б") ||
+                         line.Contains("в") ||
+                         line.Contains("г") ||
+                         line.Contains("д") ||
+                         line.Contains("е") ||
+                         line.Contains("ё") ||
+                         line.Contains("ж") ||
+                         line.Contains("з") ||
+                         line.Contains("и") ||
+                         line.Contains("й") ||
+                         line.Contains("к") ||
+                         line.Contains("л") ||
+                         line.Contains("м") ||
+                         line.Contains("н") ||
+                         line.Contains("о") ||
+                         line.Contains("п") ||
+                         line.Contains("р") ||
+                         line.Contains("с") ||
+                         line.Contains("т") ||
+                         line.Contains("у") ||
+                         line.Contains("ф") ||
+                         line.Contains("х") ||
+                         line.Contains("ц") ||
+                         line.Contains("ч") ||
+                         line.Contains("ш") ||
+                         line.Contains("щ") ||
+                         line.Contains("ь") ||
+                         line.Contains("ы") ||
+                         line.Contains("ъ") ||
+                         line.Contains("э") ||
+                         line.Contains("ю") ||
+                         line.Contains("я")))
+                    {
+                        tmp += tmp2 + Environment.NewLine;
+                    }
                 }
             }
-            streamReader.Close();
             return tmp;
+        }
+        public void Writer(string path, string dirString, System.Windows.Controls.TextBox textBox)
+        {
+            var streamReader = new StreamReader(dirString);
+            CheckExcel();
+            string tmp = Output(streamReader);
+            if (tmp != string.Empty)
+            {
+                var streamWriter = new StreamWriter(path + ".txt", true);
+                streamWriter.WriteLine(Path.GetFullPath(dirString) + "\t" + tmp);
+                streamWriter.Close();
+                //ExportToExcel(path,dirString,tmp);
+                textBox.Text += dirString + Environment.NewLine;
+            }
+            streamReader.Close();
+            KillExcel();
+        }
+
+        public void ExportToExcel(string path, string dirString, string tmp)
+        {
+            var ExcelApp = new Excel.Application();
+
+                try
+                {
+                    ExcelApp.Workbooks.Open(path + ".xlsx");
+                }
+                catch (COMException)
+                {
+                    ExcelApp.Workbooks.Add();
+                }
+                Excel._Worksheet workSheet = ExcelApp.ActiveSheet;
+                int row = ExcelApp.Sheets[1].Cells.SpecialCells(Excel.XlCellType.xlCellTypeLastCell).Row;
+                row++;
+                workSheet.Cells[row, "A"] = Path.GetFullPath(dirString);
+                if (tmp.Contains("\n"))
+                {
+                    string[] subStrings = tmp.Split('\n');
+                    foreach (var substring in subStrings)
+                    {
+                        workSheet.Cells[row, "B"] = substring;
+                        row++;
+                    }
+                }
+                workSheet.Columns.AutoFit();
+                ExcelApp.ActiveWorkbook.SaveAs(path + ".xlsx", Excel.XlFileFormat.xlOpenXMLWorkbook, System.Reflection.Missing.Value, System.Reflection.Missing.Value, System.Reflection.Missing.Value, System.Reflection.Missing.Value,
+                    Excel.XlSaveAsAccessMode.xlShared, System.Reflection.Missing.Value, System.Reflection.Missing.Value, System.Reflection.Missing.Value, System.Reflection.Missing.Value, System.Reflection.Missing.Value);
+                ExcelApp.ActiveWorkbook.Close(false);
+                ExcelApp.Quit();
+                Marshal.ReleaseComObject(workSheet);
+                Marshal.ReleaseComObject(ExcelApp);
+        }
+        public void CheckExcel()
+        {
+            Process[] AllProcesses = Process.GetProcessesByName("excel");
+            _myHashtable = new Hashtable();
+            int iCount = 0;
+
+            foreach (Process ExcelProcess in AllProcesses)
+            {
+                _myHashtable.Add(ExcelProcess.Id, iCount);
+                iCount = iCount + 1;
+            } 
+        }
+        public void KillExcel()
+        {
+
+
+            Process[] AllProcesses = Process.GetProcessesByName("excel");
+
+            // check to kill the right process
+            foreach (Process ExcelProcess in AllProcesses)
+            {
+                if (_myHashtable.ContainsKey(ExcelProcess.Id) == false)
+                    ExcelProcess.Kill();
+            }
+
+            AllProcesses = null;
         }
     }
 }
