e64e5b94df8ebb17d16a61735983c85d1ea016ce
paxan222 <paxan222@yandex.ru>
2016-06-23 16:49:42 +0300

    update

--- LocalizationParser/LocalizationParser/MainWindow.xaml.cs
+++ LocalizationParser/LocalizationParser/MainWindow.xaml.cs
@@ -4,9 +4,9 @@ using System.Diagnostics;
 using System.IO;
 using System.Runtime.InteropServices;
 using System.Text.RegularExpressions;
+using System.Web.Caching;
 using System.Windows;
 using System.Windows.Forms;
-using System.Windows.Media.TextFormatting;
 using Excel=Microsoft.Office.Interop.Excel;
 
 namespace LocalizationParser
@@ -35,7 +35,7 @@ namespace LocalizationParser
         
         private void Button_Click_1(object sender, RoutedEventArgs e)
         {
-            string[] dirStrings = Directory.GetFiles(FileProperties.Path,"*.cs", SearchOption.AllDirectories);
+            string[] dirStrings = Directory.GetFiles(FileProperties.Path,FileProperties.Extension, SearchOption.AllDirectories);
             TextBox.Text = string.Empty;
             var parse = new Parse();
             var directoryInfo = new DirectoryInfo(FileProperties.RootPath);
@@ -52,121 +52,136 @@ namespace LocalizationParser
                 parse.Writer(path,dirString,TextBox);
             }
         }
+
+        private void ComboBox_SelectionChanged(object sender, System.Windows.Controls.SelectionChangedEventArgs e)
+        {
+            switch (ComboBox.SelectedIndex)
+            {   
+                case 0:
+                    FileProperties.Extension = "*.cs";
+                    break;
+                case 1:
+                    FileProperties.Extension = "*.xaml";
+                    break;
+            }
+        }
     }
 
     public class FileProperties
     {
         public static string Path = "D:\\repo\\Trank\\Projects\\FireAdministrator\\Modules\\StrazhModule\\";
         public static string RootPath = "D:\\repo\\LocalizedTrank\\";
+        public static string Extension = "*.cs";
         public static string FileName = string.Empty;
     }
-
     public class Parse
     {
-        Hashtable _myHashtable;
-        public string Output(StreamReader streamReader)
+        private Hashtable _myHashtable;
+
+        private readonly char[] _russianAlphabeth = "абвгдеёжзийклмнопрстуфхцчшщъыьэюяАБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ".ToCharArray();
+        private readonly char[] _englishAlphabeth = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ".ToCharArray();
+        private readonly char[] _specialSymbols = "".ToCharArray();
+
+        private string ReadFromFile(StreamReader streamReader)
         {
             string tmp = string.Empty;
             while (!streamReader.EndOfStream)
             {
-                string tmp2 = streamReader.ReadLine();
-                if (tmp2 != null)
-                {
-                    string line = tmp2.ToLower();
-                    if ((line.Contains("а") ||
-                         line.Contains("б") ||
-                         line.Contains("в") ||
-                         line.Contains("г") ||
-                         line.Contains("д") ||
-                         line.Contains("е") ||
-                         line.Contains("ё") ||
-                         line.Contains("ж") ||
-                         line.Contains("з") ||
-                         line.Contains("и") ||
-                         line.Contains("й") ||
-                         line.Contains("к") ||
-                         line.Contains("л") ||
-                         line.Contains("м") ||
-                         line.Contains("н") ||
-                         line.Contains("о") ||
-                         line.Contains("п") ||
-                         line.Contains("р") ||
-                         line.Contains("с") ||
-                         line.Contains("т") ||
-                         line.Contains("у") ||
-                         line.Contains("ф") ||
-                         line.Contains("х") ||
-                         line.Contains("ц") ||
-                         line.Contains("ч") ||
-                         line.Contains("ш") ||
-                         line.Contains("щ") ||
-                         line.Contains("ь") ||
-                         line.Contains("ы") ||
-                         line.Contains("ъ") ||
-                         line.Contains("э") ||
-                         line.Contains("ю") ||
-                         line.Contains("я")))
-                    {
-                        tmp += tmp2 + Environment.NewLine;
-                    }
-                }
+                string line = streamReader.ReadLine();
+                if ((line != null) && (line.IndexOfAny(_russianAlphabeth) >=0))
+                    tmp += line + Environment.NewLine;
             }
             return tmp;
+        }
+
+        private string CutString(string input)
+        {
+            string output = string.Empty;
+            int start = 0;
+            int stop = 0;
+            int index = input.IndexOfAny(_russianAlphabeth) - 1;
+            if (input[index] == '"')
+                start = index + 1;
+            index = input.LastIndexOfAny(_russianAlphabeth);
+            int index2 = input.IndexOfAny(_englishAlphabeth, index);
+            if (index2 >= 0)
+            {
+                stop = input.LastIndexOf('"', index2, index2 - index);
+            }
+            else
+            {
+
+                stop = input.LastIndexOf('"');
+            }
+            if(stop > start)
+                output = input.Substring(start, stop - start);
+            return output;
         }
         public void Writer(string path, string dirString, System.Windows.Controls.TextBox textBox)
         {
             var streamReader = new StreamReader(dirString);
             CheckExcel();
-            string tmp = Output(streamReader);
+            string tmp = ReadFromFile(streamReader);
             if (tmp != string.Empty)
             {
                 var streamWriter = new StreamWriter(path + ".txt", true);
                 streamWriter.WriteLine(Path.GetFullPath(dirString) + "\t" + tmp);
                 streamWriter.Close();
-                ExportToExcel(path,dirString,tmp);
+                ExportToExcel(path, dirString, tmp);
                 textBox.Text += dirString + Environment.NewLine;
             }
             streamReader.Close();
             KillExcel();
         }
-        public void ExportToExcel(string path, string dirString, string tmp)
+
+        private void ExportToExcel(string path, string dirString, string tmp)
         {
             var ExcelApp = new Excel.Application();
 
-                try
+            try
+            {
+                ExcelApp.Workbooks.Open(path + ".xlsx");
+            }
+            catch (COMException)
+            {
+                ExcelApp.Workbooks.Add();
+            }
+            Excel._Worksheet workSheet = ExcelApp.ActiveSheet;
+            int row = ExcelApp.Sheets[1].Cells.SpecialCells(Excel.XlCellType.xlCellTypeLastCell).Row;
+            row++;
+            if (tmp.Contains("\n"))
+            {
+                string[] subStrings = tmp.Split('\n');
+                foreach (var substring in subStrings)
                 {
-                    ExcelApp.Workbooks.Open(path + ".xlsx");
-                }
-                catch (COMException)
-                {
-                    ExcelApp.Workbooks.Add();
-                }
-                Excel._Worksheet workSheet = ExcelApp.ActiveSheet;
-                int row = ExcelApp.Sheets[1].Cells.SpecialCells(Excel.XlCellType.xlCellTypeLastCell).Row;
-                row++;
-                workSheet.Cells[row, "A"] = Path.GetFullPath(dirString);
-                if (tmp.Contains("\n"))
-                {
-                    string[] subStrings = tmp.Split('\n');
-                    foreach (var substring in subStrings)
+                    string s = Regex.Replace(substring, @"\t", "");
+                    if (s != string.Empty)
                     {
-                        string s = Regex.Replace(substring,@"\t", "");
-                        if (!s.StartsWith("//"))
+                        if (!s.StartsWith("//") && (!s.StartsWith("#"))&&
+                            (!s.StartsWith("Logger.")))
                         {
-                            workSheet.Cells[row, "B"] = s;
+                            workSheet.Cells[row, "A"] = Path.GetFullPath(dirString);
+                            workSheet.Cells[row, "B"] = Path.GetFileNameWithoutExtension(dirString);
+                            workSheet.Cells[row, "C"] = s;
+                            workSheet.Cells[row, "D"] = CutString(s);
                             row++;
                         }
                     }
                 }
-                workSheet.Columns.AutoFit();
-                ExcelApp.ActiveWorkbook.SaveAs(path + ".xlsx", Excel.XlFileFormat.xlOpenXMLWorkbook, System.Reflection.Missing.Value, System.Reflection.Missing.Value, System.Reflection.Missing.Value, System.Reflection.Missing.Value,
-                    Excel.XlSaveAsAccessMode.xlShared, System.Reflection.Missing.Value, System.Reflection.Missing.Value, System.Reflection.Missing.Value, System.Reflection.Missing.Value, System.Reflection.Missing.Value);
-                ExcelApp.ActiveWorkbook.Close(false);
-                ExcelApp.Quit();
-                Marshal.ReleaseComObject(workSheet);
-                Marshal.ReleaseComObject(ExcelApp);
+            }
+            workSheet.Columns.AutoFit();
+            ExcelApp.ActiveWorkbook.SaveAs(path + ".xlsx", Excel.XlFileFormat.xlOpenXMLWorkbook,
+                System.Reflection.Missing.Value, System.Reflection.Missing.Value, System.Reflection.Missing.Value,
+                System.Reflection.Missing.Value, Excel.XlSaveAsAccessMode.xlShared, System.Reflection.Missing.Value,
+                System.Reflection.Missing.Value, System.Reflection.Missing.Value, System.Reflection.Missing.Value,
+                System.Reflection.Missing.Value);
+            ExcelApp.ActiveWorkbook.Close(false);
+            ExcelApp.Quit();
+            Marshal.ReleaseComObject(workSheet);
+            Marshal.ReleaseComObject(ExcelApp);
         }
-        public void CheckExcel()
+
+        private void CheckExcel()
         {
             Process[] AllProcesses = Process.GetProcessesByName("excel");
             _myHashtable = new Hashtable();
@@ -176,9 +191,10 @@ namespace LocalizationParser
             {
                 _myHashtable.Add(ExcelProcess.Id, iCount);
                 iCount = iCount + 1;
-            } 
+            }
         }
-        public void KillExcel()
+
+        private void KillExcel()
         {
             Process[] AllProcesses = Process.GetProcessesByName("excel");
 
